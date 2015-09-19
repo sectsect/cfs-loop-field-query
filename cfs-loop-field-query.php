e1b8b154a294841cb401ebdc4cb6c5a1a67c4233
SECT <info@sectwebstudio.com>
2015-09-19 17:40:40 +0900

    Conforming to PHP coding standards
    Level ”PSR-2"

--- cfs-loop-field-query.php
+++ cfs-loop-field-query.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 Plugin Name: CFS Loop-Field Query for Events
 Plugin URI: https://github.com/sectsect/cfs-loop-field-query
@@ -9,7 +10,7 @@ Author URI: http://www.ilovesect.com/
 */
 
 global $wpdb;
-define('TABLE_NAME', $wpdb->prefix . 'cfs_loop_field_query');
+define('TABLE_NAME', $wpdb->prefix.'cfs_loop_field_query');
 
 define('CFS_LFQ_POST_TYPE', get_option('cfs_lfq_posttype'));
 define('CFS_LFQ_TAXONOMY', get_option('cfs_lfq_taxonomy'));
@@ -18,16 +19,17 @@ define('CFS_LFQ_CFS_LOOP_DATE', get_option('cfs_lfq_datefield'));
 define('CFS_LFQ_CFS_LOOP_STARTTIME', get_option('cfs_lfq_starttimefield'));
 define('CFS_LFQ_CFS_LOOP_FINISHTIME', get_option('cfs_lfq_finishtimefield'));
 
-if(is_admin()){
-    register_activation_hook( __FILE__, 'cfs_lfq_activate' );
+if (is_admin()) {
+    register_activation_hook(__FILE__, 'cfs_lfq_activate');
 }
-function cfs_lfq_activate() {
+function cfs_lfq_activate()
+{
     global $wpdb;
     $cfs_lfq_db_version = '1.0';
-    $installed_ver      = get_option( 'cfs_lfq_version' );
-    $charset_collate    = $wpdb->get_charset_collate();
-    if( $installed_ver != $cfs_lfq_db_version ) {
-        $sql = "CREATE TABLE " . TABLE_NAME . " (
+    $installed_ver = get_option('cfs_lfq_version');
+    $charset_collate = $wpdb->get_charset_collate();
+    if ($installed_ver != $cfs_lfq_db_version) {
+        $sql = 'CREATE TABLE '.TABLE_NAME." (
               event_id bigint(20) NOT NULL AUTO_INCREMENT,
               post_id bigint(20) NOT NULL,
               date date NOT NULL,
@@ -35,87 +37,91 @@ function cfs_lfq_activate() {
               finishtime time,
               PRIMARY KEY  (event_id, post_id)
             ) $charset_collate;";
-        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        require_once ABSPATH.'wp-admin/includes/upgrade.php';
         dbDelta($sql);
         update_option('cfs_lfq_version', $cfs_lfq_db_version);
     }
 }
 
 /*==================================================
-	Save to "wp_cfs_loop_field_query" table.
+    Save to "wp_cfs_loop_field_query" table.
 ================================================== */
-function save_event($params) {
-	if(get_post_type($params['post_data']['ID']) == CFS_LFQ_POST_TYPE && CFS()->get(CFS_LFQ_CFS_LOOP, $params['post_data']['ID'])){
-		global $wpdb;
-		$postID    = $params['post_data']['ID'];
-		$fields    = CFS()->get(CFS_LFQ_CFS_LOOP, $postID);
+function save_event($params)
+{
+    if (get_post_type($params['post_data']['ID']) == CFS_LFQ_POST_TYPE && CFS()->get(CFS_LFQ_CFS_LOOP, $params['post_data']['ID'])) {
+        global $wpdb;
+        $postID = $params['post_data']['ID'];
+        $fields = CFS()->get(CFS_LFQ_CFS_LOOP, $postID);
 
-		$sql       = "DELETE FROM " . TABLE_NAME . " WHERE post_id = $postID;";
-		$sql       = $wpdb->prepare($sql);
-		$result    = $wpdb->query($sql);
+        $sql = 'DELETE FROM '.TABLE_NAME." WHERE post_id = $postID;";
+        $sql = $wpdb->prepare($sql);
+        $result = $wpdb->query($sql);
 
-		foreach($fields as $field){
-			$date   = str_replace("-", "", $field[CFS_LFQ_CFS_LOOP_DATE]);
+        foreach ($fields as $field) {
+            $date = str_replace('-', '', $field[CFS_LFQ_CFS_LOOP_DATE]);
 
-            if($field[CFS_LFQ_CFS_LOOP_STARTTIME]){
-                $stime  = str_replace(":", "", $field[CFS_LFQ_CFS_LOOP_STARTTIME].":00");
-            }else{
-                $stime  = 'null';
+            if ($field[CFS_LFQ_CFS_LOOP_STARTTIME]) {
+                $stime = str_replace(':', '', $field[CFS_LFQ_CFS_LOOP_STARTTIME].':00');
+            } else {
+                $stime = 'null';
             }
-            if($field[CFS_LFQ_CFS_LOOP_FINISHTIME]){
-                $ftime  = str_replace(":", "", $field[CFS_LFQ_CFS_LOOP_FINISHTIME].":00");
-            }else{
-                $ftime  = 'null';
+            if ($field[CFS_LFQ_CFS_LOOP_FINISHTIME]) {
+                $ftime = str_replace(':', '', $field[CFS_LFQ_CFS_LOOP_FINISHTIME].':00');
+            } else {
+                $ftime = 'null';
             }
-			$sql    = "INSERT INTO " . TABLE_NAME . " (post_id, date, starttime, finishtime) VALUES ($postID, $date, $stime, $ftime);";
-			$sql    = $wpdb->prepare($sql);
-			$result = $wpdb->query($sql);
-		}
-	}
+            $sql = 'INSERT INTO '.TABLE_NAME." (post_id, date, starttime, finishtime) VALUES ($postID, $date, $stime, $ftime);";
+            $sql = $wpdb->prepare($sql);
+            $result = $wpdb->query($sql);
+        }
+    }
 }
 add_action('cfs_after_save_input', 'save_event');
 
 /*==================================================
-	Delete the data in "wp_cfs_loop_field_query" table.
+    Delete the data in "wp_cfs_loop_field_query" table.
 ================================================== */
-add_action( 'before_delete_post', 'delete_event' );
-function delete_event($postID) {
-	if(get_post_type($postID) == CFS_LFQ_POST_TYPE && CFS()->get(CFS_LFQ_CFS_LOOP, $postID)){
-		global $wpdb;
-		$sql    = "DELETE FROM " . TABLE_NAME . " WHERE post_id = $postID;";
-		$sql    = $wpdb->prepare($sql);
-		$result = $wpdb->query($sql);
-	}
+add_action('before_delete_post', 'delete_event');
+function delete_event($postID)
+{
+    if (get_post_type($postID) == CFS_LFQ_POST_TYPE && CFS()->get(CFS_LFQ_CFS_LOOP, $postID)) {
+        global $wpdb;
+        $sql = 'DELETE FROM '.TABLE_NAME." WHERE post_id = $postID;";
+        $sql = $wpdb->prepare($sql);
+        $result = $wpdb->query($sql);
+    }
 }
 
 /*==================================================
-	Modify the Main Query
+    Modify the Main Query
 ================================================== */
-function cfs_lfq_pre_get_posts( $query ) {
+function cfs_lfq_pre_get_posts($query)
+{
     // if( !empty($query->query_vars['calendar']) ){
-	// 	$venue = $query->get('calendar');
-	// 	$query->set('calendar',$venue);
-	// }
+    // 	$venue = $query->get('calendar');
+    // 	$query->set('calendar',$venue);
+    // }
 
-	if(is_admin() || !$query->is_main_query())
-		return;
+    if (is_admin() || !$query->is_main_query()) {
+        return;
+    }
 
-	// if(!is_user_logged_in()){	//	For "Preview" on status: future/draft/private
-	// 	$query->set('post_status', 'publish');
-	// }
+    // if(!is_user_logged_in()){	//	For "Preview" on status: future/draft/private
+    // 	$query->set('post_status', 'publish');
+    // }
 
-	if($query->is_post_type_archive(CFS_LFQ_POST_TYPE)){
-		$query->set('posts_per_page', 3);
+    if ($query->is_post_type_archive(CFS_LFQ_POST_TYPE)) {
+        $query->set('posts_per_page', 3);
 
         add_filter('posts_fields', 'event_fields', 10, 2);
-		add_filter('posts_join', 'event_join', 10, 2);
-		add_filter('posts_where', 'event_where', 10, 2);
-		add_filter('posts_orderby', 'event_orderby', 10, 2);
+        add_filter('posts_join', 'event_join', 10, 2);
+        add_filter('posts_where', 'event_where', 10, 2);
+        add_filter('posts_orderby', 'event_orderby', 10, 2);
     //    add_filter('posts_groupby', 'event_groupby', 10, 2);        // ========== 投稿の重複出力を切る ==========
-	}
+    }
 
-    if(CFS_LFQ_TAXONOMY){
-        if($query->is_tax(CFS_LFQ_TAXONOMY)){
+    if (CFS_LFQ_TAXONOMY) {
+        if ($query->is_tax(CFS_LFQ_TAXONOMY)) {
             add_filter('posts_fields', 'event_fields', 10, 2);
             add_filter('posts_join', 'event_join', 10, 2);
             add_filter('posts_where', 'event_where', 10, 2);
@@ -124,24 +130,28 @@ function cfs_lfq_pre_get_posts( $query ) {
         }
     }
 }
-add_action( 'pre_get_posts', 'cfs_lfq_pre_get_posts');
+add_action('pre_get_posts', 'cfs_lfq_pre_get_posts');
 
 /*==================================================
-	Add query_vars for calendar
+    Add query_vars for calendar
 ================================================== */
-function cfs_lfq_register_query_vars( $qvars ){
-	$qvars[] = 'calendar';
-	return $qvars;
+function cfs_lfq_register_query_vars($qvars)
+{
+    $qvars[] = 'calendar';
+
+    return $qvars;
 }
-add_filter('query_vars', 'cfs_lfq_register_query_vars' );
+add_filter('query_vars', 'cfs_lfq_register_query_vars');
 /*==================================================
     Modify & Setting the Sub Query @ http://bradt.ca/blog/extending-wp_query/
 ================================================== */
-class CFS_LFQ_Query extends WP_Query {
-	function __construct( $args = array() ) {
-		$args = array_merge( $args, array(
-			'post_type' => CFS_LFQ_POST_TYPE
-		) );
+class CFS_LFQ_Query extends WP_Query
+{
+    public function __construct($args = array())
+    {
+        $args = array_merge($args, array(
+            'post_type' => CFS_LFQ_POST_TYPE,
+        ));
         /*==================================================
             Remove the add_filter('pre_get_posts').
         ================================================== */
@@ -154,19 +164,19 @@ class CFS_LFQ_Query extends WP_Query {
         remove_filter('posts_groupby', 'event_groupby', 10, 2);
 
         add_filter('posts_fields', 'event_fields', 10, 2);
-    	add_filter('posts_join', 'event_join', 10, 2);
-        if($args['calendar']){
+        add_filter('posts_join', 'event_join', 10, 2);
+        if ($args['calendar']) {
             add_filter('posts_where', 'calendar_where', 10, 2);
-        }else{
+        } else {
             add_filter('posts_where', 'event_where', 10, 2);
         }
-    	add_filter('posts_orderby', 'event_orderby', 10, 2);
+        add_filter('posts_orderby', 'event_orderby', 10, 2);
         add_filter('post_limits', 'event_limits', 10, 2);
     //    add_filter('posts_groupby', 'event_groupby',10, 2);
 
-		parent::__construct( $args );
+        parent::__construct($args);
 
-		// Make sure these filters don't affect any other queries
+        // Make sure these filters don't affect any other queries
         remove_filter('posts_fields', 'event_fields', 10, 2);
         remove_filter('posts_join', 'event_join', 10, 2);
         remove_filter('posts_where', 'event_where', 10, 2);
@@ -174,79 +184,86 @@ class CFS_LFQ_Query extends WP_Query {
         remove_filter('posts_orderby', 'event_orderby', 10, 2);
         remove_filter('post_limits', 'event_limits', 10, 2);
     //    remove_filter('posts_groupby', 'event_groupby', 10, 2);
-	}
+    }
 }
 /*==================================================
     functions
 ================================================== */
-function event_fields( $select ){
-	global $wpdb;
-	$select = "* ";
+function event_fields($select)
+{
+    global $wpdb;
+    $select = '* ';
 
-	return $select;
+    return $select;
 }
 
-function event_join( $join ){
+function event_join($join)
+{
     global $wpdb;
-    $join = "LEFT JOIN " . TABLE_NAME . " ON {$wpdb->posts}.ID = " . TABLE_NAME . ".post_id";
+    $join = 'LEFT JOIN '.TABLE_NAME." ON {$wpdb->posts}.ID = ".TABLE_NAME.'.post_id';
 
     return $join;
 }
 
-function event_where( $where ){
-    if(!is_date()){
+function event_where($where)
+{
+    if (!is_date()) {
         // If you have set the 'finishtime', it does not appear that post when it passes your set time. (Default: the day full)
-        if(!CFS_LFQ_CFS_LOOP_FINISHTIME){
-            $today = date_i18n("Ymd");
-            $where = " AND post_type = '". CFS_LFQ_POST_TYPE . "' AND post_status = 'publish' AND date >= $today ";
-        }else{
-            $currenttime = date_i18n("YmdHis");
-            $where = " AND post_type = '". CFS_LFQ_POST_TYPE . "' AND post_status = 'publish' AND TIMESTAMP(date,finishtime) > $currenttime ";
+        if (!CFS_LFQ_CFS_LOOP_FINISHTIME) {
+            $today = date_i18n('Ymd');
+            $where = " AND post_type = '".CFS_LFQ_POST_TYPE."' AND post_status = 'publish' AND date >= $today ";
+        } else {
+            $currenttime = date_i18n('YmdHis');
+            $where = " AND post_type = '".CFS_LFQ_POST_TYPE."' AND post_status = 'publish' AND TIMESTAMP(date,finishtime) > $currenttime ";
         }
-
-    }else{
-        if(is_year()){
-            $theyaer   = get_query_var('year');
-            $startday  = $theyaer . "-01-01";
-            $finishday = $theyaer . "-12-31";
-            $where     = " AND post_type = '". CFS_LFQ_POST_TYPE . "' AND post_status = 'publish' AND date BETWEEN '$startday' AND '$finishday' ";
+    } else {
+        if (is_year()) {
+            $theyaer = get_query_var('year');
+            $startday = $theyaer.'-01-01';
+            $finishday = $theyaer.'-12-31';
+            $where = " AND post_type = '".CFS_LFQ_POST_TYPE."' AND post_status = 'publish' AND date BETWEEN '$startday' AND '$finishday' ";
         }
-        if(is_month()){
-            $themonth  = get_query_var('year')."-".get_query_var('monthnum');
-            $startday  = $themonth . "-01";
-            $finishday = $themonth . "-31";
-            $where     = " AND post_type = '". CFS_LFQ_POST_TYPE . "' AND post_status = 'publish' AND date BETWEEN '$startday' AND '$finishday' ";
+        if (is_month()) {
+            $themonth = get_query_var('year').'-'.get_query_var('monthnum');
+            $startday = $themonth.'-01';
+            $finishday = $themonth.'-31';
+            $where = " AND post_type = '".CFS_LFQ_POST_TYPE."' AND post_status = 'publish' AND date BETWEEN '$startday' AND '$finishday' ";
         }
-        if(is_day()){
+        if (is_day()) {
             $thedate = get_query_var('year').'-'.get_query_var('monthnum').'-'.get_query_var('day');
-            $theday  = date_i18n('Ymd', strtotime($thedate));
-            $where   = " AND post_type = '". CFS_LFQ_POST_TYPE . "' AND post_status = 'publish' AND date = $theday ";
+            $theday = date_i18n('Ymd', strtotime($thedate));
+            $where = " AND post_type = '".CFS_LFQ_POST_TYPE."' AND post_status = 'publish' AND date = $theday ";
         }
     }
 
     return $where;
 }
 
-function event_orderby( $orderby ){
-    return "date, post_id ASC";
+function event_orderby($orderby)
+{
+    return 'date, post_id ASC';
 }
 
-function event_limits( $limit ){
+function event_limits($limit)
+{
     return $limit;
 }
 
-function event_groupby( $groupby ){
+function event_groupby($groupby)
+{
     global $wpdb;
-    if(is_post_type_archive(CFS_LFQ_POST_TYPE)){        //  is_post_type_archive の場合、重複投稿を出力させないため、groupbyでくくる
+    if (is_post_type_archive(CFS_LFQ_POST_TYPE)) {        //  is_post_type_archive の場合、重複投稿を出力させないため、groupbyでくくる
         $groupby = "{$wpdb->posts}.ID";
     }
+
     return $groupby;
 }
 
 // ========== For calendar ========== (Date starts from first day in the month.)
-function calendar_where( $where ){
-    $today = date_i18n("Ym"."01");
-    $where = " AND post_type = '". CFS_LFQ_POST_TYPE . "' AND post_status = 'publish' AND date >= $today ";
+function calendar_where($where)
+{
+    $today = date_i18n('Ym'.'01');
+    $where = " AND post_type = '".CFS_LFQ_POST_TYPE."' AND post_status = 'publish' AND date >= $today ";
 
     return $where;
 }
@@ -256,37 +273,43 @@ function calendar_where( $where ){
 ================================================== */
 $cfs_time_picker_addon = new cfs_time_picker_addon();
 
-class cfs_time_picker_addon{
-    function __construct() {
+class cfs_time_picker_addon
+{
+    public function __construct()
+    {
         add_filter('cfs_field_types', array($this, 'cfs_field_types'));
     }
 
-    function cfs_field_types( $field_types ) {
-        $field_types['time_picker'] = dirname( __FILE__ ) . '/cfs-time/time.php';
+    public function cfs_field_types($field_types)
+    {
+        $field_types['time_picker'] = dirname(__FILE__).'/cfs-time/time.php';
+
         return $field_types;
     }
 }
 
 /*==================================================
-	Add Menu Page
+    Add Menu Page
 ================================================== */
 add_action('admin_menu', 'cfs_lfq_menu');
-function cfs_lfq_menu() {
-    add_options_page('CFS Loop Field Query', 'CFS Loop Field Query', 8, 'cfs_lfq_menu', 'cfs_lfq_options_page');		// 第三引数： 2 （管理者〜寄稿者）	5 （管理者〜編集者）	8 （管理者のみ）
-	add_action('admin_init', 'register_cfs_lfq_settings');
+function cfs_lfq_menu()
+{
+    add_options_page('CFS Loop Field Query', 'CFS Loop Field Query', 8, 'cfs_lfq_menu', 'cfs_lfq_options_page');        // 第三引数： 2 （管理者〜寄稿者）	5 （管理者〜編集者）	8 （管理者のみ）
+    add_action('admin_init', 'register_cfs_lfq_settings');
 }
-function register_cfs_lfq_settings() {
-	register_setting('cfs_lfq-settings-group', 'cfs_lfq_posttype');
+function register_cfs_lfq_settings()
+{
+    register_setting('cfs_lfq-settings-group', 'cfs_lfq_posttype');
     register_setting('cfs_lfq-settings-group', 'cfs_lfq_taxonomy');
-	register_setting('cfs_lfq-settings-group', 'cfs_lfq_dategroup');
-	register_setting('cfs_lfq-settings-group', 'cfs_lfq_datefield');
+    register_setting('cfs_lfq-settings-group', 'cfs_lfq_dategroup');
+    register_setting('cfs_lfq-settings-group', 'cfs_lfq_datefield');
     register_setting('cfs_lfq-settings-group', 'cfs_lfq_starttimefield');
     register_setting('cfs_lfq-settings-group', 'cfs_lfq_finishtimefield');
 }
-function cfs_lfq_options_page() {
-	require_once(plugin_dir_path( __FILE__ ) . "admin/index.php");
+function cfs_lfq_options_page()
+{
+    require_once plugin_dir_path(__FILE__).'admin/index.php';
 }
-
 
 /*==================================================
     Native code Example
@@ -318,6 +341,4 @@ function cfs_lfq_options_page() {
 //     $title	  = $value->post_title;
 //     array_push($ary, array('date' => $date, 'id' => $post_id, 'permlink' => $perm, 'title' => $title));
 // }
-
-
-?>
+;
